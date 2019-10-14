98e70f009c3e741b013654a571980ac25fddbfe3
joelethan <joelkatsme@gmail.com>
2019-10-14 08:53:52 -0700

    JWT athentication

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const user = require('./routes/api/user')
+const profile = require('./routes/api/profile')
+const passport = require('passport')
+
+require('dotenv').config()
+
+const app = express()
+
+// Body parsermiddleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Connect to db
+console.log(process.env.connectionString);
+
+mongoose.connect(process.env.connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=>console.log('MongoDB Connected'))
+    .catch(err => console.log(err))
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+app.get('/', (req, res)=> res.send('Hello world!!'))
+
+// Use routes
+app.use('/api/user', user)
+app.use('/api/profile', profile)
+
+app.listen(5000, ()=>console.log('Listening to port 5000'))
