a7044d3baea9e8e98e660a0d8d674531c89212e3
joelethan <joelkatsme@gmail.com>
2019-10-22 14:44:28 -0700

    Add education background to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,7 @@ const Profile = require('../../models/Profile')
 const User = require('../../models/User')
 const validateProfileInput = require('../../validation/profile')
 const validateExperienceInput = require('../../validation/experience')
+const validateEducationInput = require('../../validation/education')
 
 // Test route
 // api/profile/test
@@ -160,6 +161,34 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
             }
         // Add to experience array
         profile.experiences.unshift(newExp)
+        profile.save()
+            .then(profile => res.json(profile))
+        })
+})
+
+// Add education to the profile
+// api/profile/education
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res)=> {
+    const { errors, isValid } = validateEducationInput(req.body)
+
+    if(!isValid){
+        // Return errors
+        return res.status(400).json(errors)
+    }
+
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            const newEdu = {
+                school: req.body.school,
+                degree: req.body.degree,
+                fieldofstudy: req.body.fieldofstudy,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description
+            }
+        // Add to education array
+        profile.education.unshift(newEdu)
         profile.save()
             .then(profile => res.json(profile))
         })
