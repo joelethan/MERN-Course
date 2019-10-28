884049ee8675c87debbe6ff37e507a6713211b78
joelethan <joelkatsme@gmail.com>
2019-10-28 13:33:11 -0700

    Delete a single post

--- routes/api/post.js
+++ routes/api/post.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose')
 const passport = require('passport')
 
-// load Post model
+// load Post and Profile models
 const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
 
 // import validation
 const validatePostInput = require('../../validation/post')
@@ -15,7 +16,7 @@ router.get('/test', (req, res)=> res.json({msg: 'Working posts'}))
 
 // Create post
 // private
-router.post('/',passport.authenticate('jwt', { session: false }), (req, res) => {
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
     const { errors, isValid } = validatePostInput(req.body)
 
     if(!isValid){
@@ -48,6 +49,23 @@ router.get('/:id', (req, res) => {
     Post.findById(req.params.id)
         .then(post => res.json(post))
         .catch(err => res.status(404).json({errors:'Post not found'}))
+})
+
+// Get a post
+// private
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({user: req.user.id})
+        .then(profile => {
+            if(!profile) return res.status(404).json({noprofile: 'Profile not found'})
+            Post.findOne({_id: req.params.id})
+                .then(post => {
+                    if(post.user.toString() !== profile.user.toString()) {
+                        return res.status(401).json({ notauthorised: 'User not authorised'})
+                    }
+                    post.remove().then(()=> res.json({msg: 'Deleted successfully'}))
+                })
+                .catch(err => res.status(404).json({nopost: 'Post not found'}))
+            })
 })
 
 module.exports = router;
