d8df59e410e5892d88ea5aa4b518806f05743ea4
joelethan <joelkatsme@gmail.com>
2019-11-06 13:45:49 -0800

    - Install axios - Implement registration from the form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Register extends Component {
     constructor(){
@@ -18,7 +19,16 @@ class Register extends Component {
     }
     onSubmit(e){
         e.preventDefault();
-        console.log(this.state);
+        const newUser = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2
+        }
+        
+        axios.post('/api/user/register', newUser)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err.response.data))
     }
     render() {
         return (
