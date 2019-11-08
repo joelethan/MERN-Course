a7a8130fd6957977b110b0b3b57164863d54b236
joelethan <joelkatsme@gmail.com>
2019-11-08 08:44:39 -0800

    Redux and chrome extension setup

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,26 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
  
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar/>
-        <Route exact path="/" component={ Landing } />
-        <div className="container">
-          <Route exact path="/register" component={ Register } />
-          <Route exact path="/login" component={ Login } />
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <Route exact path="/" component={ Landing } />
+          <div className="container">
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </div>
+          <Footer/>
         </div>
-        <Footer/>
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
