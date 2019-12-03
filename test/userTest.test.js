46506d8bcd208a9127b491d152db3cfb7765ae1c
joelethan <joelkatsme@gmail.com>
2019-12-03 10:41:31 -0800

    Add coveralls.yml

--- test/userTest.test.js
+++ test/userTest.test.js
@@ -3,8 +3,8 @@ process.env.NODE_ENV = 'test';
 const expect = require('chai').expect;
 const request = require('supertest');
 
-const app = require('../../server');
-const conn = require('../../db');
+const app = require('../server');
+const conn = require('../db');
 
 describe('USER /api/user/', ()=>{
     const userData = {
